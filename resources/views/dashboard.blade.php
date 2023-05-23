94e7f9b82e4e9accdf64507bfd2bb5ceaf0cd48f
hhuhhuhhuhh <carloschult1996@gmail.com>
2023-05-23 21:16:53 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,7 @@
 <x-app-layout>
     <div class="DashboardBox">
-        <h1>Sch</h1>
-        <div> A </div>
-        <button onclick="location.href='/ship/show'" class="StartButton">Start G</button>
+        <h1>Schiffe versenken</h1>
+        <button onclick="location.href='/ship/show'" class="StartButton">Start Game</button>
         <button onclick="location.href='/ship/Tutorial'" class="StartButton">Tutorial</button>
     </div>
 </x-app-layout>
